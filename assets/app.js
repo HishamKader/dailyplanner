ca6fb9513f3352ee6ca1e8f1295b0513dd1fe12b
Hisham Kader <hisham319@hotmail.com>
2020-10-16 18:51:21 -0400

    added javascript

--- assets/app.js
+++ assets/app.js
@@ -1,77 +1,79 @@
 // get our document ready for jQuery.
-
-
-// Declare our time and date variables using moment.js
-
-
-
-
-
-
-// Store our necessary DOM elements to variables.
+$(document).ready(function () {
+    // Declare our time and date variables using moment.js
+    var todaysDate = moment().format('dddd MMM Do YYYY');
+    var timeNow = moment().format('hh:mm a');
+    var timeCompare = moment().format('H');
+    var times = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 
 
+    // Store our necessary DOM elements to variables.
+    var currentDayEl = $("#currentDay");
+    var timeBlocks = $(".timeBlock");
+    var saveBtn = $(".saveBtn");
+    var timeLiEl = $("li");
+    var inputArea = $(".inputArea");
 
+    // declare our global variables.
+    var timeEntered; textEnterd;
 
+    // Place our current date in the jumbotron in the UI.
+    currentDayEl.text(todaysDate);
 
-     // declare our global variables.
+    colorCode();
 
+    /*
+           colorCode() function determines if a timeslot is past,
+           future, or present and colors the block accordingly.
+           Red ("#ff7675") will appear for the current time block.
+           Green ("#55efc4") will appear for future time blocks.
+           Previous time blocks stay the default grey.
+   */
+    function colorCode() {
+        // Determine which colors to show for each times lot.
+        for (var i = 0; i < timeliEl.length; i++) {
+            // if the current time is equal to a timeblock time make it green.
+            if (timeCompare == times[i]) {
+                inputArea[i].style.backgroundColor = "#ff7675";
+            }
+            // if the current time is less than a timeblock time make it red.
+            else if (timeComapare < times[i]) {
+                inputArea[i].style.backgroundColor = "#55efc4"
+            }
+        };
+    }
 
-// Place our current date in the jumbotron in the UI.
-
-
-
-
- /*
-        colorCode() function determines if a timeslot is past,
-        future, or present and colors the block accordingly.
-        Red ("#ff7675") will appear for the current time block.
-        Green ("#55efc4") will appear for future time blocks.
-        Previous time blocks stay the default grey.
-*/
-
-// Determine which colors to show for each times lot.
-
- // if the current time is equal to a timeblock time make it green.
-
-
-
-// if the current time is less than a timeblock time make it red.
-
-
-
-
-
-
- // Cycle through local storage, if an entry exists show it in the 
+    // Cycle through local storage, if an entry exists show it in the 
     // correct timeslot.
+    for (var i = 9; i <= 17; i++) {
+        $((".") + i).val(localStorage["" + i]);
+        console.log(localStorage["" + i]);
+    }
 
+    // Define what happens when our user clicks the "saveBtn"
+    $("ul").on("click", ".saveBtn", function (event) {
 
-
-
-
-// Define what happens when our user clicks the "saveBtn"
-
-
-    // Stop our submit button from deleting the textarea data.
-
+        // Stop our submit button from deleting the textarea data.
+        event.preventDefualt();
         // Save the inputBox value.
-
+        var inputBox = ($(this))
 
         // Get the timeslot value which corresponds to the time.\
-
+        var inputBox = ($(this).prev().val());
         // store our inputBox and timeSlot vars in global variables.
-        
-
+        textEntered = inputBox;
+        timeEntered = timeSlot;
         // Log to the console which timeslot was just saved.
-
+        console.log("User saved to " + timeEntered + " time slot");
 
         // Update localStorage to include our recently input values.
+        localStorage.setItem(timeEntered, textEnterd);
+        var PreviousPost = localStorage.getItem(textEntered);
+    });
 
 
-
-
+});
 
 
 
